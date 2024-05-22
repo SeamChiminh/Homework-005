03e4089f68dbdd7d0538b73cb432a8ff92371ca3
seamchiminh <chiminh096@gmail.com>
2024-05-22 08:11:15 +0700

    create class Course

---
+++ src/model/Course.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.Date;
+
+public class Course {
+    private Integer id;
+    private String title;
+    private String[] instructorName;
+    private String[] requirement;
+    private Date startDate ;
+}
